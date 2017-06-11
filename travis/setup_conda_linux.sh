386d5dbb65f4f8fe5ae1984fa588082d7526e951
Jonathan Barnoud <jonathan@barnoud.net>
2017-06-11 15:40:39 +0200

    Allows to install 32 bits conda on linux
    The 32 bits conda is installed if the ARCHITECTURE_32BIT environment
    variable is set to True.
    
    The code is copied from <https://github.com/obspy/obspy/pull/1580>.

--- travis/setup_conda_linux.sh
+++ travis/setup_conda_linux.sh
@@ -13,9 +13,25 @@ fi
 
 echo "==================== Starting executing ci-helpers scripts ====================="
 
+if [[ ${ARCHITECTURE_32BIT} == True ]]; then
+	export ARCH=""
+	# OPT is used by setuptools patches contained in newer numpy versions
+	# used by obspy when running pip install --no-deps .
+	export OPT="-m32"
+	# workaround for sudo dpkg --add-architecture i386
+	# be sure multiarch is the only file in this directory
+	ls /etc/dpkg/dpkg.cfg.d/
+	# multiarch must contain foreign-architecture i386. If not it will
+	# error later and you need to do the following and add sudo: true
+	#sudo sh -c "echo 'foreign-architecture i386' > /etc/dpkg/dpkg.cfg.d/multiarch"
+	cat /etc/dpkg/dpkg.cfg.d/multiarch
+else
+	export ARCH="_64"
+fi
+
 # Install conda
 # http://conda.pydata.org/docs/travis.html#the-travis-yml-file
-wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86${ARCH}.sh -O miniconda.sh
 bash miniconda.sh -b -p $HOME/miniconda
 export PATH="$HOME/miniconda/bin:$PATH"
 
